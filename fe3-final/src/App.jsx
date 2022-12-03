c6164f3ce2cdc1c85db960f1f0bf47c69fd51db1
GeraldineBrussa <geraldine.r.brussa@gmail.com>
2022-12-03 07:51:52 -0300

    Subiendo a mi branch punto 1-rutas

--- fe3-final/src/App.jsx
+++ fe3-final/src/App.jsx
@@ -1,15 +1,32 @@
-
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import { routes } from "./routes";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <div className="App">
+    <Routes>
+      <Route path={routes.home} element={<Home />}>
+        <Route path="/dentist/:id" element={<Detail />} />
+      </Route>
+      <Route path={routes.contact} element={<Contact />} />
+      <Route path={routes.fav} element={<Favs />} />
+    </Routes>
+  );
+}
+export default App;
+
+/* Éste div con los componenetes Navbar y Footer estaban en el documento original pero se supone que en app sólo van las rutas, avisame si te parece bien sacarlo o lo dejamos pero en teoría dejarlo no sería una buena práctica.
+<div className="App">
           <Navbar/>
           <Footer/>
       </div>
-  );
-}
 
-export default App;
+      En la consigna no hay un not Found, lo podríamos poner si nos sobra el tiempo
+
+      <Route path="*" element={<NotFound />} />
+*/
--- fe3-final/src/Routes/Home.jsx
+++ fe3-final/src/Routes/Home.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import Card from '../Components/Card'
+import React from "react";
+import Card from "../Components/Card";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
   return (
-    <main className="" >
+    <main className="">
+      <Navbar />
       <h1>Home</h1>
-      <div className='card-grid'>
+      <div className="card-grid">
         {/* Aqui deberias renderizar las cards */}
       </div>
+      <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
