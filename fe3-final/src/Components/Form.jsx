8ea08b8bbc8535875027de83b70f85c2e6520e90
GeraldineBrussa <geraldine.r.brussa@gmail.com>
2022-12-10 11:16:18 -0300

    Volviendo al form original porque no se pudo implementar bien con el CostumHooK

--- fe3-final/src/Components/Form.jsx
+++ fe3-final/src/Components/Form.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import useInput from "./UseInput";
 
 const Form = () => {
-  const fullName = useInput("text");
-  const email = useInput("email");
+  const [user, setUser] = useState({
+    fullName: "",
+    email: "",
+    fullNameSuccesMessage: "",
+  });
 
   const [message, setMessage] = useState({
     errorMessage: "",
@@ -18,7 +20,7 @@ const Form = () => {
     3: "Name needs at least 5 letters",
     4: "Email is empty",
     5: "Email is invalid",
-    6: "We received your information and we're going to contact you!",
+    6: " we received your information and we're going to contact you",
   };
 
   const emailRegex =
@@ -29,41 +31,57 @@ const Form = () => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if (fullName.value.length <= 0) {
+    if (user.fullName.length <= 0) {
       return setMessage((prev) => ({ ...prev, errorMessage: messages[1] }));
-    } else if (!fullName.value.match(fullNameRegex)) {
+    } else if (!user.fullName.match(fullNameRegex)) {
       return setMessage((prev) => ({ ...prev, errorMessage: messages[2] }));
-    } else if (fullName.value.length <= 4) {
+    } else if (user.fullName.length <= 4) {
       return setMessage((prev) => ({ ...prev, errorMessage: messages[3] }));
-    } else if (email.value.length <= 0) {
+    } else if (user.email.length <= 0) {
       return setMessage((prev) => ({ ...prev, errorMessage: messages[4] }));
-    } else if (!email.value.match(emailRegex)) {
+    } else if (!user.email.match(emailRegex)) {
       return setMessage((prev) => ({ ...prev, errorMessage: messages[5] }));
     } else {
       setMessage((prev) => ({ ...prev, succesMessage: messages[6] }));
 
       setMessage((prev) => ({ ...prev, numberMessage: 6 }));
 
+      setUser((prev) => ({ ...prev, fullNameSuccesMessage: user.fullName }));
+
       setMessage((prev) => ({ ...prev, errorMessage: messages[0] }));
+
+      setUser((prev) => ({ ...prev, fullName: messages[0] }));
+      setUser((prev) => ({ ...prev, email: messages[0] }));
     }
-    document.getElementById("formContact").reset();
   };
 
   return (
     <>
       <fieldset>
         <legend>Please fill out the form below</legend>
-        <form id="formContact" onSubmit={onSubmit}>
-          <input {...fullName} placeholder="Full Name" />
-          <input {...email} placeholder="Email" />
-          <button type="onsubmit">Send</button>
+        <form onSubmit={onSubmit}>
+          <input
+            value={[user.fullName]}
+            type="text"
+            name="fullName"
+            placeholder="Full Name"
+            onChange={(e) => setUser({ ...user, fullName: e.target.value })}
+          />
+          <input
+            value={[user.email]}
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <button type="submit">Send</button>
         </form>
       </fieldset>
       <p className="error" style={{ color: "red" }}>
         {message.errorMessage}
       </p>
       <p className="success" style={{ color: "green" }}>
-        {message.numberMessage == 6 ? "Hi " + fullName.value : ""}
+        {message.numberMessage == 6 ? "Hi " + user.fullNameSuccesMessage : ""}
         {message.succesMessage}
       </p>
     </>
